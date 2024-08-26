e029a5b67a3fcc2cbeff4d5d2659cacda78dad0f
neerajgandhii <neerajgandhii2003@gmail.com>
2024-08-26 11:48:01 +0530

    day2

---
+++ arithmeticOperators.java
@@ -0,0 +1,11 @@
+public class arithmeticOperators{
+    public static void main(String[] args) {
+        int A = 12;
+        int B = 5;
+        System.out.println("Sum = " + (A + B));
+        System.out.println("Difference = " + (A - B));
+        System.out.println("Product = " + (A * B));
+        System.out.println("Division = " + (A / B));
+        System.out.println("Remainder = " + (A % B));
+    }
+}
