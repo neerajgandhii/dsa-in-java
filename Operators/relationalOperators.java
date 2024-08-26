a46284cecf1a96faf238a4873c91bb0aba59acbb
neerajgandhii <neerajgandhii2003@gmail.com>
2024-08-26 12:08:21 +0530

    day2

---
+++ Operators/relationalOperators.java
@@ -0,0 +1,17 @@
+public class relationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+
+        int c = 10;
+        int d = 10;
+        System.out.println(c<d);
+        System.out.println(c>d);
+        System.out.println(c<=d);
+        System.out.println(c>=d);
+    }
+}
