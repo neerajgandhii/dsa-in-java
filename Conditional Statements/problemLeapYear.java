33e3ac1f64d5405dc13cfe99814dc6a20f3b07d8
neerajgandhii <neerajgandhii2003@gmail.com>
2024-08-29 22:27:07 +0530

    day3

---
+++ Conditional Statements/problemLeapYear.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class problemLeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a year");
+        int year = sc.nextInt();
+
+        if (year > 0 && year % 4 == 0){
+            if (year % 400 == 0){
+                System.out.println(year + " is a leap year.");
+            }
+            else if (year % 100 == 0){
+                System.out.println(year + " is not a leap year.");
+            }
+            else{
+                System.out.println(year + " is a leap year.");
+            }
+        }
+        else{
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
