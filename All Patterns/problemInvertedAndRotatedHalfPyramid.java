f3f336e3f83a8396f6005bd80ea5750b0f52cddc
neerajgandhii <neerajgandhii2003@gmail.com>
2024-09-08 23:38:05 +0530

    day11

---
+++ All Patterns/problemInvertedAndRotatedHalfPyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class problemInvertedAndRotatedHalfPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Choose a size for your pattern");
+        int size = sc.nextInt();
+
+        for (int pattern = 1; pattern <= size; pattern++){
+            for (int blanks = 1; blanks <= size - 1; blanks++){
+                System.out.print(" ");
+            }
+            for (int stars = 1; stars <= size; stars++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
