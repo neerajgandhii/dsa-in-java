a4b9b6a4222f3498e009d828fed7718dd9560f92
neerajgandhii <neerajgandhii2003@gmail.com>
2024-09-24 23:58:41 +0530

    day22

--- Scratched My Brain/keywordsCounter.java
+++ Scratched My Brain/keywordsCounter.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class keywordsCounter {
   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("Enter your text");
+    String text = sc.nextLine();
+
+    System.out.println("Enter the keyword you wish to search");
+    String keyword = sc.next();
   }
 }
