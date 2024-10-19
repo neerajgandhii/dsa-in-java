4233699ae69fa7d643c1aa9c557fcc9c62d68668
Neeraj <neerajgandhii2003@gmail.com>
2024-10-19 23:59:59 +0530

    day27

---
+++ Functions/helloWorld.java
@@ -0,0 +1,12 @@
+public class helloWorld{
+
+  public static void printHelloWorld(){
+    System.out.println("Hello World");
+    System.out.println("Hello");
+    System.out.println("...");
+  }
+
+  public static void main(String args[]){
+    printHelloWorld();
+  }
+}
---
+++ Functions/sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class sum{
+
+  public static int calculateSum(int a, int b){
+    int sum = a + b;
+    return sum;
+  }
+
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    int sum = 
+  }
+}
